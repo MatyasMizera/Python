f1fbc4a5f87208475f6aed3aa3e722ad724a3c69
Matyáš  Mizera <151766959+MatyasMizera@users.noreply.github.com>
2024-01-08 17:14:20 +0100

    Add files via upload

---
+++ prg10 generování charakteru.py
@@ -0,0 +1,39 @@
+print("Tvoje postava:")
+import random
+n = (random.randint(1,3))
+if n == 1:
+    print("má černý vlasy",'')
+elif n == 2:
+    print("má hnědý vlasy")
+elif n == 3:
+    print("má bílý vlasy")
+
+import random
+n = (random.randint(1,3))
+if n == 1:
+    print("je vysoká")
+elif n == 2:
+    print("je středně vysoká")
+elif n == 3:
+    print("je malá")
+
+import random
+n = (random.randint(1,2))
+if n == 1:
+    print("je tlustá")
+elif n == 2:
+    print("je hubená")
+
+import random
+n = (random.randint(1,2))
+if n == 1:
+    print("je slabá")
+elif n == 2:
+    print("je silná")
+
+import random
+n = (random.randint(1,2))
+if n == 1:
+    print("je rychlá")
+elif n == 2:
+    print("je pomalá")
