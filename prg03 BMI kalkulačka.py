8d235001ebdd9302bdf97a1a1275824547934ab2
Matyáš  Mizera <151766959+MatyasMizera@users.noreply.github.com>
2025-10-24 09:41:16 +0200

    Update prg03 BMI kalkulačka.py

--- prg03 BMI kalkulačka.py
+++ prg03 BMI kalkulačka.py
@@ -1,17 +1,23 @@
-vyska = 175
-vaha = 76
+vyska = float(input("Zadej výšku v cm: "))
+vaha = float(input("Zadej váhu v kg: "))
+
 vyska = vyska / 100
 bmi = vaha / (vyska * vyska)
+
 if bmi < 18.5:
-    print("podváha")
+    print("Podváha")
 elif bmi > 18.5 and bmi < 24.9:
-    print("normální váha")
+    print("Normální váha")
 elif bmi > 24.9 and bmi < 29.9:
-     print("Nadváha")
+    print("Nadváha")
 elif bmi > 29.9 and bmi < 34.9:
-     print("Obezita 1.stupně")
+    print("Obezita 1. stupně")
 elif bmi > 34.9 and bmi < 39.9:
-     print("Obezita 2.stupně")
+    print("Obezita 2. stupně")
 elif bmi > 39.9 and bmi < 100:
-     print("Obezita 3.stupně")
+    print("Obezita 3. stupně")
+
+print("BMI:",(bmi))
+
      
+
